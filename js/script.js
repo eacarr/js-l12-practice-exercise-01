d5c5257bdc8b0be952de8a9be6f31675f70e225f
Everette Carrera <everettecarrera@gmail.com>
2021-12-09 14:57:52 -0600

    add phone number

--- js/script.js
+++ js/script.js
@@ -1,40 +1,36 @@
 const randomFolks = document.querySelector(".random-peeps");
 const selectUserNumber = document.querySelector("#users");
+
 const getData = async (numUsers) => {
   const usersRequest = await fetch(
     `https://randomuser.me/api?results=${numUsers}`
   );
   const data = await usersRequest.json();
-  //   console.log(usersRequest);
-  //   console.log(data);
   const userResults = data.results;
-  //   console.log(userResults);
+  console.log(data.results);
   displayUsers(userResults);
 };
-
 getData(1);
-//user items to display
 
 const displayUsers = (userResults) => {
-  randomFolks.innerHTML = "";
+  randomFolks.textContent = "";
   for (let user of userResults) {
-    const country = user.location.country;
-    const name = user.name.first;
-    const imageUrl = user.picture.medium;
-    const userDiv = document.createElement("div");
+    let country = user.location.country;
+    let name = user.name.first;
+    let imageUrl = user.picture.medium;
+    let phone = user.phone;
+    let userDiv = document.createElement("div");
     userDiv.innerHTML = `
         <h3>${name}</h3>
         <p>${country}</p>
         <img src=${imageUrl} alt="User avatar" />
+        <a href="tel:${phone}">${phone}
     `;
     randomFolks.append(userDiv);
   }
-  //   console.log(displayUsers);
 };
 
-// remove hide from num-users class
-
 selectUserNumber.addEventListener("change", (e) => {
-  const numUsers = e.target.value;
+  let numUsers = e.target.value;
   getData(numUsers);
 });
